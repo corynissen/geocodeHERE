03333e7e0cca552b4388e74d5856d212f15fe43b
corynissen <corynissen@gmail.com>
2014-10-23 09:19:08 -0500

    removed the format option

--- R/geocodeHERE_simple.R
+++ R/geocodeHERE_simple.R
@@ -1,44 +1,34 @@
 
-#' Read in App_id and App_code
-#'
-#' Loads a file that contains the App_id and App_code
-#' @param file_name The location of the file
-#' @keywords App_id App_code
-#' @export
-#' @examples
-#' \dontrun{
-#' ids = get_IDs(file_name="~/nokia_HERE.txt")
-#' geocodeHERE_simple("chicago")
-#' geocodeHERE_simple("wrigley field chicago IL")
-#' geocodeHERE_simple("233 S Wacker Dr, Chicago, IL 60606")
-#' }
-#' get_IDs
-get_IDs <- function(file_name="R/nokia_HERE.txt"){
-  ids <- scan(file_name, what="character", quiet=T)
-  return(list(App_id=ids[2], App_code=ids[4]))
-}
-
 #' Attempt to geocode a string
 #'
 #' Enter a string and have latitude and longitude returned using the HERE API
 #' @param search A string to search
-#' @param format json or xml. Defaults to json.
-#' @param ids The output from the get_IDs function. This contains the App_id and App_code required for authentication by the HERE API
+#' @param App_id App_id to use the production HERE API. Get one here... http://developer.here.com/get-started. If left blank, will default to demo key with an unknown usage limit.
+#' @param App_code App_code to use the production HERE API. Get one here... http://developer.here.com/get-started. If left blank, will default to demo key with an unknown usage limit.
 #' @keywords geocode
 #' @export
 #' @examples
 #' \dontrun{
-#' ids = get_IDs(file_name="~/nokia_HERE.txt")
 #' geocodeHERE_simple("chicago")
 #' geocodeHERE_simple("wrigley field chicago IL")
 #' geocodeHERE_simple("233 S Wacker Dr, Chicago, IL 60606")
 #' }
 #' geocodeHERE_simple
-geocodeHERE_simple <- function(search, format="json", ids=ids){
-  # get the lat lon for somewhere...
-  #ids <- get_IDs()
-  base_url <- "http://geocoder.api.here.com/6.2/geocode."
+geocodeHERE_simple <- function(search, App_id="", App_code=""){
+  if(!is.character(search)){stop("'search' must be a character string")}
+  if(!is.character(App_id)){stop("'App_id' must be a character string")}
+  if(!is.character(App_code)){stop("'App_code' must be a character string")}
+
+  if(App_id=="" & App_code==""){
+    App_id <- "DemoAppId01082013GAL"
+    App_code <- "AJKnXv84fjrb0KIHawS0Tg"
+    base_url <- "http://geocoder.cit.api.here.com/6.2/geocode."
+  }else{
+    base_url <- "http://geocoder.api.here.com/6.2/geocode."
+  }
+
   search <- RCurl::curlEscape(search)
+
   final_url <- paste0(base_url, format, "?app_id=", ids$App_id, "&app_code=",
                       ids$App_code, "&searchtext=", search)
 
@@ -52,5 +42,4 @@ geocodeHERE_simple <- function(search, format="json", ids=ids){
   return(ret)
 }
 
-#ids <- get_IDs()
-#geocodeHERE_simple("the bean chicago", ids=ids)
+
