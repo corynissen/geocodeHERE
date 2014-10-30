aaaf1e865fc8dec89f24250047f427e7890c81be
corynissen <corynissen@gmail.com>
2014-10-30 11:13:21 -0500

    no more reading from file, just read addresses from an R object

--- R/geocodeHERE_batch_upload.R
+++ R/geocodeHERE_batch_upload.R
@@ -2,7 +2,7 @@
 #' Upload a file to Nokia HERE batch geocoding API
 #'
 #' Upload a file to Nokia HERE batch geocoding API
-#' @param file_name Character string containing the path to the file to be uploaded
+#' @param address_string Character string containing the addresses to be geocoded. Output from 'format_vec_for_upload(...)
 #' @param email_address Character string containing an email address. Nokia will email you here when the job is done.
 #' @param App_id App_id to use the production HERE API. Get one here... http://developer.here.com/get-started. If left blank, will default to demo key with an unknown usage limit.
 #' @param App_code App_code to use the production HERE API. Get one here... http://developer.here.com/get-started. If left blank, will default to demo key with an unknown usage limit.
@@ -11,16 +11,19 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' geocodeHERE_batch_upload(file_name = "example.txt", email_address = "youremail<at>domain.com")
+#' #' data(chicago_landmarks)
+#' addresses <- chicago_landmarks[,"Address"]
+#' addresses <- paste(addresses, "chicago IL")
+#' address_str <- format_vec_for_upload(addresses)
+#' geocodeHERE_batch_upload(address_string = address_str, email_address = "youremail<at>domain.com")
 #' }
 #' geocodeHERE_batch_upload
-geocodeHERE_batch_upload <- function(file_name, email_address, App_id="",
+geocodeHERE_batch_upload <- function(address_string, email_address, App_id="",
                                      App_code="", quiet=TRUE){
-  if(!is.character(file_name)){stop("'file_name' must be a character string")}
+  if(!is.character(address_string)){stop("'address_string' must be a character string")}
   if(!is.character(email_address)){stop("'file_name' must be a character string")}
   if(!is.character(App_id)){stop("'App_id' must be a character string")}
   if(!is.character(App_code)){stop("'App_code' must be a character string")}
-  if(!file.exists(file_name)){stop("File does not exist, check your path or working directory")}
 
   if(App_id=="" & App_code==""){
     App_id <- "DemoAppId01082013GAL"
@@ -31,11 +34,8 @@ geocodeHERE_batch_upload <- function(file_name, email_address, App_id="",
   }
 
   v <- ifelse(quiet, httr::verbose(), NULL)
-
-  bod <- paste(readLines(file_name, warn=F), collapse="\n")
-
   a <- httr::POST(base_url, encode="multipart",
-            body=bod,
+            body=address_string,
             query=list(
               action="run",
               mailto=email_address,
