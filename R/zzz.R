4d6fac1e4783b49ee6f97bbae4de69b8b9d4fc0a
corynissen <corynissen@gmail.com>
2014-10-30 14:36:30 -0500

    change startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,4 @@
 
 .onLoad <- function(lib, pkg, ...) {
-  packageStartupMessage("testing onLoad function")
-  demo_geocode_base_url <<- "http://geocoder.cit.api.here.com/6.2/geocode.json"
-  prod_geocode_base_url <- "http://geocoder.api.here.com/6.2/geocode.json"
-
-  demo_batch_geocode_base_url <- "http://batch.geocoder.cit.api.here.com/6.2/jobs"
-  prod_batch_geocode_base_url <- "http://batch.geocoder.api.here.com/6.2/jobs"
-
-  demo_App_id <- "DemoAppId01082013GAL"
-  demo_App_code <- "AJKnXv84fjrb0KIHawS0Tg"
+  packageStartupMessage("This package is very experimental and may change frequently.")
 }
